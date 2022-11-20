14c8aec3e196f049d774705934c3685278326087
Sebastian Bergman <basse@grafikfel.org>
2022-11-20 01:41:18 +0100

    use example info in seed comment

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,14 +2,13 @@ import { prisma } from "../src/server/db/client";
 
 async function main() {
   // const basse = await prisma.user.upsert({
-  //   where: { email: "basse@grafikfel.org" },
+  //   where: { email: "name@example.com" },
   //   update: {},
   //   create: {
-  //     email: "basse@grafikfel.org",
-  //     name: "Basse",
+  //     email: "name@example.com",
+  //     name: "Name",
   //   },
   // });
-  // console.log({ basse });
 }
 
 main()
