717ff90ba7f258ef4afd3da24b5be66e72e218df
Sebastian Bergman <basse@grafikfel.org>
2022-12-03 11:34:01 +0100

    try using staleTime Infinity

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -14,6 +14,13 @@ const getBaseUrl = () => {
 export const trpc = createTRPCNext<AppRouter>({
   config() {
     return {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+          },
+        },
+      },
       transformer: superjson,
       links: [
         loggerLink({
