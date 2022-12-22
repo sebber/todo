971e534a0cbcca41b7d3f6160612ac3987730420
Sebastian Bergman <basse@grafikfel.org>
2022-12-22 02:15:40 +0100

    feat: optimistic update todo toggle

--- src/modules/todos/queries.ts
+++ src/modules/todos/queries.ts
@@ -47,20 +47,12 @@ function updateTodoList(
   ];
 }
 
-export function useToggleTodo(todoListId: string) {
-  return trpc.todo.toggle.useMutation();
-}
-
-export function useEditTodoText(todoListId: string) {
-  return trpc.todo.changeTodoText.useMutation();
-}
-
-export function useClearCompletedTodos() {
-  return trpc.todo.clearCompleted.useMutation();
-}
-
 export function useCreateTodoList() {
   return trpc.todo.createTodoList.useMutation();
+}
+
+export function useDeleteTodoList() {
+  return trpc.todo.deleteTodoList.useMutation();
 }
 
 export function useChangeTodoListTitle() {
@@ -119,6 +111,49 @@ export function useAddTodo(todoListId: string) {
   });
 }
 
-export function useDeleteTodoList() {
-  return trpc.todo.deleteTodoList.useMutation();
+type TodoUpdateAction = (todo: Todo) => Todo;
+
+function updateTodo(todos: Todo[], todoId: string, update: TodoUpdateAction) {
+  if (todos?.length <= 0) return todos;
+  const index = todos?.findIndex((list) => list.id === todoId);
+  if (index === -1) return todos;
+  const todo = todos[index];
+  if (!todo) return todos;
+
+  return [...todos.slice(0, index), update(todo), ...todos.slice(index + 1)];
+}
+
+export function useToggleTodo(todoListId: string) {
+  const utils = trpc.useContext();
+  return trpc.todo.toggle.useMutation({
+    async onMutate(variables) {
+      await utils.todo.getTodoLists.cancel();
+      const previousData = utils.todo.getTodoLists.getData();
+
+      utils.todo.getTodoLists.setData(undefined, (todoLists = []) => {
+        return updateTodoList(todoLists, todoListId, (list) => ({
+          ...list,
+          todos: updateTodo(list.todos, variables.id, (todo) => ({
+            ...todo,
+            done: !todo.done,
+          })),
+        }));
+      });
+
+      return { previousData };
+    },
+    onError(_err, _var, context) {
+      if (context?.previousData) {
+        utils.todo.getTodoLists.setData(undefined, context.previousData);
+      }
+    },
+  });
+}
+
+export function useEditTodoText(todoListId: string) {
+  return trpc.todo.changeTodoText.useMutation();
+}
+
+export function useClearCompletedTodos() {
+  return trpc.todo.clearCompleted.useMutation();
 }
