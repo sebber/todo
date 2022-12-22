1f5737b2e91db27431cf86a43ff322cf73df55d0
Sebastian Bergman <basse@grafikfel.org>
2022-12-23 00:23:40 +0100

    chore: remove the staletime Infinite

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -32,13 +32,6 @@ export const trpc = createTRPCNext<AppRouter>({
   },
   config() {
     return {
-      queryClientConfig: {
-        defaultOptions: {
-          queries: {
-            staleTime: Infinity,
-          },
-        },
-      },
       transformer: superjson,
       links: [
         loggerLink({
