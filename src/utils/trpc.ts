76fe93106df16e074b9548eeea76cc04d5a4fdb7
Sebastian Bergman <basse@grafikfel.org>
2022-12-30 14:15:32 +0100

    test: don't invalidate everything after any mutation

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -26,10 +26,11 @@ export const trpc = createTRPCNext<AppRouter>({
         // Calls the `onSuccess` defined in the `useQuery()`-options:
         await opts.originalFn();
         // Invalidate all queries in the react-query cache:
-        await opts.queryClient.invalidateQueries();
+        // await opts.queryClient.invalidateQueries();
       },
     },
   },
+
   config() {
     return {
       transformer: superjson,
